741efb9fc741cf8aa405d2161854b58cef02d0d3
Xiao Junfeng <353906237@qq.com>
2019-10-09 09:37:13 +0800

    tutorial

---
+++ src/test/java/jar/AppTest.java
@@ -0,0 +1,13 @@
+package jar;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest{
+
+    public void testadd()
+    {
+        assertTrue(Calc.add(3,2)==5);
+    }
+}
